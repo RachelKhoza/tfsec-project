141bf3a13b8ce2d387b0f1225e6ce00ef2c6213b
Rachel Khoza <malete.rachel@gmail.com>
2024-03-03 19:57:13 +0200

    adding workflows

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "mybuck_ray" {
+    bucket = "raytest_buck12321221"
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket_acl"{
+    bucket = aws_s3_bucket.mybuck_ray.id
+    rule{
+        object_ownership = "BucketOwner"
+    }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucket_acl]
+   
+  bucket = aws_s3_bucket.mybuck_ray.id
+  acl = "private"
+}
